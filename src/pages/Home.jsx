8ce52f21a3c9295de58f24df2aa49e43356b7d06
Igboanugwo Collins <munisco12@gmail.com>
2023-03-27 09:12:15 +0100

    installed react-icons and added search icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,23 +79,7 @@ const Gallery = () => {
 const first_list = ['Home', 'About', 'Contacts', 'Services', 'Feedback']
 const second_list = ['Braids', 'Wigs', 'Hair', 'Makeup', 'Nails']
 
-const NavList = ({ list }) => {
-    return (
-        <ul className='header__ul'>
-            {/* Loop through the array, and make each item to be list, also the key is very important in react to differenciate the individual items, thus it must be unique */}
-            {list.map((nav, idx) => (
-                <li className='header__li' key={idx + nav}>
-                    <Link
-                        to={'#'}
-                        className={`header__link ${idx === 0 && 'active-link'}`}
-                    >
-                        {nav}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    )
-}
+
 
 const NavListContainer = ({ navList }) => {
     return (

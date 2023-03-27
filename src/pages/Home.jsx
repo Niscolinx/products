c1e43f565cd752fc7dee65d72da6b4b08cfd2ef5
Igboanugwo Collins <munisco12@gmail.com>
2023-03-27 08:50:57 +0100

    working on nav list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -106,81 +106,85 @@ const NavListContainer = ({navList}) => {
     )
 }
 
+const Header = () => {
+
+    return (
+        <section className='header'>
+            <NavListContainer navList={first_list} />
+
+            <div className='logo'>
+                <div className='logo-box'>
+                    <div>
+                        <img
+                            src='/img/logo2.png'
+                            alt='logo'
+                            className='logo-logo'
+                        />
+                    </div>
+                </div>
+
+                <div className='search-box'>
+                    <form className='search-form'>
+                        <input
+                            type='text'
+                            name='search'
+                            placeholder='&#x1F50D;'
+                            className='search-input'
+                        />
+
+                        <button className='search-btn'>Search</button>
+                    </form>
+                </div>
+            </div>
+
+            <div className='secondary'>
+                <nav className='secondary__nav'>
+                    <ul className='secondary__ul'>
+                        <li className='secondary__li'>
+                            <a href='braids.html' className='secondary__link'>
+                                Braids
+                            </a>
+                        </li>
+                        <li className='secondary__li'>
+                            <a href='#' className='secondary__link'>
+                                Wigs
+                            </a>
+                        </li>
+                        <li className='secondary__li'>
+                            <a href='hair.html' className='secondary__link'>
+                                Hair
+                            </a>
+                        </li>
+                        <li className='secondary__li'>
+                            <a href='#' className='secondary__link'>
+                                Makeup
+                            </a>
+                        </li>
+                        <li className='secondary__li'>
+                            <a href='nails.html' className='dark__link'>
+                                Nails
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className='logo-contact'>
+                    <p className='logo-contact__p'>
+                        <i className='logo-contact__icon fa-solid fa-phone-volume'></i>
+                        +2348136016162
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
 const ProductCard = () => {}
 
 function Home() {
     return (
         <div>
-            <section className='header'>
-                <NavListContainer navList={first_list}/>
-
-                <div className='logo'>
-                    <div className='logo-box'>
-                        <div>
-                            <img
-                                src='/img/logo2.png'
-                                alt='logo'
-                                className='logo-logo'
-                            />
-                        </div>
-                    </div>
-
-                    <div className='search-box'>
-                        <form className='search-form'>
-                            <input
-                                type='text'
-                                name='search'
-                                placeholder='&#x1F50D;'
-                                className='search-input'
-                            />
-
-                            <button className='search-btn'>Search</button>
-                        </form>
-                    </div>
-                </div>
-
-                <div className='secondary'>
-                    <nav className='secondary__nav'>
-                        <ul className='secondary__ul'>
-                            <li className='secondary__li'>
-                                <a
-                                    href='braids.html'
-                                    className='secondary__link'
-                                >
-                                    Braids
-                                </a>
-                            </li>
-                            <li className='secondary__li'>
-                                <a href='#' className='secondary__link'>
-                                    Wigs
-                                </a>
-                            </li>
-                            <li className='secondary__li'>
-                                <a href='hair.html' className='secondary__link'>
-                                    Hair
-                                </a>
-                            </li>
-                            <li className='secondary__li'>
-                                <a href='#' className='secondary__link'>
-                                    Makeup
-                                </a>
-                            </li>
-                            <li className='secondary__li'>
-                                <a href='nails.html' className='dark__link'>
-                                    Nails
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-
-                    <div className='logo-contact'>
-                        <p className='logo-contact__p'>
-                            <i className='logo-contact__icon fa-solid fa-phone-volume'></i>
-                            +2348136016162
-                        </p>
-                    </div>
-                </div>
-            </section>
+            <Header/>
             <div class='section2'>
                 <div class='line'></div>
 

4bc506dc825199d482f203dbe8e5e1aaaafd1a13
Igboanugwo Collins <munisco12@gmail.com>
2023-03-27 09:15:36 +0100

    installed react-icons and added search icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
+const NavList = ({ list }) => {
+    return (
+        <ul className='header__ul'>
+            {/* Loop through the array, and make each item to be list, also the key is very important in react to differenciate the individual items, thus it must be unique */}
+            {list.map((nav, idx) => (
+                <li className='header__li' key={idx + nav}>
+                    <Link
+                        to={'#'}
+                        className={`header__link ${idx === 0 && 'active-link'}`}
+                    >
+                        {nav}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+
 const Header = () => {
     return (
         <section className='header'>

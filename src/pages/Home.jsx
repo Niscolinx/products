9397ff3d247b343a79bc524764438eb6f4929112
Igboanugwo Collins <munisco12@gmail.com>
2023-03-27 09:11:12 +0100

    installed react-icons and added search icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { CiSearch } from 'react-icons/ci'
+import Header from '../components/Header'
 
 const Gallery = () => {
     return (
@@ -107,79 +108,7 @@ const NavListContainer = ({ navList }) => {
     )
 }
 
-const Header = () => {
-    return (
-        <section className='header'>
-            <NavListContainer navList={first_list} />
 
-            <div className='logo'>
-                <div className='logo-box'>
-                    <div>
-                        <img
-                            src='/img/logo2.png'
-                            alt='logo'
-                            className='logo-logo'
-                        />
-                    </div>
-                </div>
-
-                <div className='searchBox'>
-                    <form className='searchBox__form'>
-                        <input
-                            type='text'
-                            name='search'
-                            placeholder='&#x1F50D;'
-                            className='searchBox__input'
-                        />
-
-                        <button className='searchBox__btn'>Search</button>
-                    </form>
-                </div>
-            </div>
-
-            <NavListContainer navList={second_list} />
-
-            <div className='secondary'>
-                <nav className='secondary__nav'>
-                    <ul className='secondary__ul'>
-                        <li className='secondary__li'>
-                            <a href='braids.html' className='secondary__link'>
-                                Braids
-                            </a>
-                        </li>
-                        <li className='secondary__li'>
-                            <a href='#' className='secondary__link'>
-                                Wigs
-                            </a>
-                        </li>
-                        <li className='secondary__li'>
-                            <a href='hair.html' className='secondary__link'>
-                                Hair
-                            </a>
-                        </li>
-                        <li className='secondary__li'>
-                            <a href='#' className='secondary__link'>
-                                Makeup
-                            </a>
-                        </li>
-                        <li className='secondary__li'>
-                            <a href='nails.html' className='dark__link'>
-                                Nails
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-
-                <div className='logo-contact'>
-                    <p className='logo-contact__p'>
-                        <i className='logo-contact__icon fa-solid fa-phone-volume'></i>
-                        +2348136016162
-                    </p>
-                </div>
-            </div>
-        </section>
-    )
-}
 
 const ProductCard = () => {}
 

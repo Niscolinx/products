7db830ec7f2036aef42d098e33cafd351f61fdc2
Igboanugwo Collins <munisco12@gmail.com>
2023-03-27 08:39:23 +0100

    changed the input to button the form, because a button inside a form automatically has a type of submit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,79 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const Gallery = () => {
+    return (
+        <div className='gallery'>
+            <div className='gallery-container'>
+                <div className='gallery-items'>
+                    <div className='gallery-box'>
+                        <img
+                            src='/img/hair.jpg.crdownload'
+                            alt=''
+                            className='gallery--1'
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className='slider'>
+                <div className='gallery-box2'>
+                    <img src='/img/bridal.jpg' alt='' className='gallery--2' />
+                    <img src='/img/afro.jpg' alt='' className='gallery--2' />
+                    <img src='/img/bride.jpg' alt='' className='gallery--2' />
+                    <img src='/img/lipie.jpg' alt='' className='gallery--2' />
+                    <img
+                        src='/img/Butterfly-locs.jpg'
+                        alt=''
+                        className='gallery--2'
+                    />
+                    <img
+                        src='/img/straight-orange-hair-bundles.jpg'
+                        alt=''
+                        className='gallery--2'
+                    />
+                </div>
+
+                <button className='slider__btn slider__btn--left'>
+                    &larr;
+                </button>
+                <button className='slider__btn slider__btn--right'>
+                    &rarr;
+                </button>
+                <div className='dots'></div>
+            </div>
+
+            <div className='gallery--3'>
+                <div className='gallery-items'>
+                    <div className='gallery-box'>
+                        <img
+                            src='/img/lipstick.jpg'
+                            alt=''
+                            className='gallery--3'
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className='gallery-box gallery--4'>
+                <img src='/img/wash.jpg' alt='' className='gallery--4' />
+            </div>
+
+            <div className='gallery-box gallery--5'>
+                <img src='/img/ped.jpg' alt='' className='gallery--5' />
+            </div>
+
+            <div className='gallery-box gallery--6'>
+                <img
+                    src='/img/beautiful-bridal-nails.jpg'
+                    alt=''
+                    className='gallery--6'
+                />
+            </div>
+        </div>
+    )
+}
+
 const FirstNavList = () => {
     //Make an array of all the nav items
     const navArr = ['Home', 'About', 'Contacts', 'Services', 'Feedback']
@@ -64,30 +137,30 @@ function Home() {
                     </div>
                 </div>
 
-                <div className='dark'>
-                    <nav className='dark__nav'>
-                        <ul className='dark__ul'>
-                            <li className='dark__li'>
-                                <a href='braids.html' className='dark__link'>
+                <div className='secondary'>
+                    <nav className='secondary__nav'>
+                        <ul className='secondary__ul'>
+                            <li className='secondary__li'>
+                                <a href='braids.html' className='secondary__link'>
                                     Braids
                                 </a>
                             </li>
-                            <li className='dark__li'>
-                                <a href='#' className='dark__link'>
+                            <li className='secondary__li'>
+                                <a href='#' className='secondary__link'>
                                     Wigs
                                 </a>
                             </li>
-                            <li className='dark__li'>
-                                <a href='hair.html' className='dark__link'>
+                            <li className='secondary__li'>
+                                <a href='hair.html' className='secondary__link'>
                                     Hair
                                 </a>
                             </li>
-                            <li className='dark__li'>
-                                <a href='#' className='dark__link'>
+                            <li className='secondary__li'>
+                                <a href='#' className='secondary__link'>
                                     Makeup
                                 </a>
                             </li>
-                            <li className='dark__li'>
+                            <li className='secondary__li'>
                                 <a href='nails.html' className='dark__link'>
                                     Nails
                                 </a>
@@ -103,94 +176,7 @@ function Home() {
                     </div>
                 </div>
 
-                <div className='gallery'>
-                    <div className='gallery-container'>
-                        <div className='gallery-items'>
-                            <div className='gallery-box'>
-                                <img
-                                    src='/img/hair.jpg.crdownload'
-                                    alt=''
-                                    className='gallery--1'
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='slider'>
-                        <div className='gallery-box2'>
-                            <img
-                                src='/img/bridal.jpg'
-                                alt=''
-                                className='gallery--2'
-                            />
-                            <img
-                                src='/img/afro.jpg'
-                                alt=''
-                                className='gallery--2'
-                            />
-                            <img
-                                src='/img/bride.jpg'
-                                alt=''
-                                className='gallery--2'
-                            />
-                            <img
-                                src='/img/lipie.jpg'
-                                alt=''
-                                className='gallery--2'
-                            />
-                            <img
-                                src='/img/Butterfly-locs.jpg'
-                                alt=''
-                                className='gallery--2'
-                            />
-                            <img
-                                src='/img/straight-orange-hair-bundles.jpg'
-                                alt=''
-                                className='gallery--2'
-                            />
-                        </div>
-
-                        <button className='slider__btn slider__btn--left'>
-                            &larr;
-                        </button>
-                        <button className='slider__btn slider__btn--right'>
-                            &rarr;
-                        </button>
-                        <div className='dots'></div>
-                    </div>
-
-                    <div className='gallery--3'>
-                        <div className='gallery-items'>
-                            <div className='gallery-box'>
-                                <img
-                                    src='/img/lipstick.jpg'
-                                    alt=''
-                                    className='gallery--3'
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='gallery-box gallery--4'>
-                        <img
-                            src='/img/wash.jpg'
-                            alt=''
-                            className='gallery--4'
-                        />
-                    </div>
-
-                    <div className='gallery-box gallery--5'>
-                        <img src='/img/ped.jpg' alt='' className='gallery--5' />
-                    </div>
-
-                    <div className='gallery-box gallery--6'>
-                        <img
-                            src='/img/beautiful-bridal-nails.jpg'
-                            alt=''
-                            className='gallery--6'
-                        />
-                    </div>
-                </div>
+               
             </section>
             <div class='section2'>
                 <div class='line'></div>
